f854dfaa9ed129d37e922b07f83b93f7063b7f41
M Bimo Bayu Bagaskara <m.bimo.bayu.bagaskara-2022@fst.unair.ac.id>
2024-09-17 09:46:39 +0700

    Object type TypeScript

---
+++ tests/object.test.ts
@@ -0,0 +1,14 @@
+describe("Test Object", () => {
+  it("should be object type", () => {
+    const bimo: { nim: string; name: string; jurusan: string } = {
+      nim: "187221049",
+      name: "M Bimo Bayu Bagaskara",
+      jurusan: "Sistem Informasi",
+    };
+
+    bimo.name = "Biyuraaa";
+    expect(bimo.nim).toBe("187221049");
+    expect(bimo.name).toBe("Biyuraaa");
+    expect(bimo.jurusan).toBe("Sistem Informasi");
+  });
+});
