e95be6034cdf8b2d894f12f17485eb08e7943eb1
M Bimo Bayu Bagaskara <m.bimo.bayu.bagaskara-2022@fst.unair.ac.id>
2024-09-18 11:28:36 +0700

    ReadOnly Properties TypeScript

--- src/mahasiswa.ts
+++ src/mahasiswa.ts
@@ -1,5 +1,11 @@
+export enum JK {
+  "LAKI-LAKI",
+  "PEREMPUAN",
+}
+
 export interface Mahasiswa {
   nim: string;
   nama: string;
   ipk: number;
+  readonly jk: JK;
 }
--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -1,4 +1,4 @@
-import { Mahasiswa } from "../src/mahasiswa";
+import { Mahasiswa, JK } from "../src/mahasiswa";
 
 describe("Test Interface", () => {
   it("should support interface", () => {
@@ -6,9 +6,22 @@ describe("Test Interface", () => {
       nim: "187221049",
       nama: "M Bimo Bayu Bagaskara",
       ipk: 3.52,
+      jk: JK["LAKI-LAKI"],
     };
 
     console.log(bimo);
+    expect(bimo.nama).toBe("M Bimo Bayu Bagaskara");
+  });
+  it("should support readonly properties", () => {
+    const bimo: Mahasiswa = {
+      nim: "187221049",
+      nama: "M Bimo Bayu Bagaskara",
+      ipk: 3.52,
+      jk: JK["LAKI-LAKI"],
+    };
+
+    console.log(bimo);
+    // bimo.jk = JK.PEREMPUAN error karena jk merupakan atribut readonly dimana jika atribut tersebut dirubah maka akan menghasilkan error
     expect(bimo.nama).toBe("M Bimo Bayu Bagaskara");
   });
 });
